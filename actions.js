cfb86b27294fc4f4989c24abdb69d7235bb8e369
DanielMoyano <61314311+DanielMoyano@users.noreply.github.com>
2026-04-13 12:46:31 +0200

    limpieza media

--- actions.js
+++ actions.js
@@ -81,5 +81,9 @@ function cargar() {
     } else {
         console.log("`lazy-loading` no soportado...");
     }
+    let images = ["imgs/vertical/arbol.png","imgs/horizontal/bayocheta.png","imgs/vertical/coche.png","imgs/cuadrado/copo.png","imgs/vertical/gato.png", "imgs/cuadrado/fuego.png","imgs/horizontal/eyes.png","imgs/vertical/blasfemo.png","imgs/horizontal/bici.png","imgs/cuadrado/mante.png", "imgs/horizontal/helldiver_sample art.png", "imgs/cuadrado/gamez.png"];
+    for (i=0; i<12;i++) {
+        document.getElementById("i"+i).src = images[i];
+    }
 
 }
